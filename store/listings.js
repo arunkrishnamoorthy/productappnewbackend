b255caf36ec47260f249e361223b8ca881ed3656
Arun Krishanmoorthy <arunmba.rec@gmail.com>
2020-09-17 21:48:21 +0530

    Backend: Initial Commit for New App

---
+++ store/listings.js
@@ -0,0 +1,123 @@
+const listings = [
+  {
+    id: 201,
+    title: "Parker Pen",
+    images: [{ fileName: "parkerpen" }],
+    price: 100,
+    categoryId: 5,
+    userId: 1,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 3,
+    title: "Dymo",
+    images: [{ fileName: "dymo" }],
+    categoryId: 1,
+    price: 1200,
+    userId: 2,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 1,
+    title: "Elmers",
+    description:
+      "I'm selling my furniture at a discount price. Pick up at Venice. DM me asap.",
+    images: [
+      { fileName: "couch1" },
+      { fileName: "couch2" },
+      { fileName: "couch3" },
+    ],
+    price: 1000,
+    categoryId: 1,
+    userId: 1,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 2,
+    title: "Mr. Sketch",
+    images: [{ fileName: "mrsketch" }],
+    categoryId: 5,
+    price: 100,
+    userId: 2,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 102,
+    title: "Papermate",
+    images: [{ fileName: "camera1" }],
+    price: 300,
+    categoryId: 3,
+    userId: 1,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 101,
+    title: "Krazy Glue",
+    images: [{ fileName: "camera2" }],
+    price: 350,
+    categoryId: 3,
+    userId: 1,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 4,
+    title: "Prisma Color",
+    description: "No rips no stains no odors",
+    images: [{ fileName: "couch3" }],
+    categoryId: 1,
+    price: 950,
+    userId: 2,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+  {
+    id: 6,
+    title: "Reynolds",
+    images: [{ fileName: "shoes2" }],
+    categoryId: 5,
+    price: 50,
+    userId: 2,
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+  },
+];
+
+const addListing = (listing) => {
+  listing.id = listings.length + 1;
+  listings.push(listing);
+};
+
+const getListings = () => listings;
+
+const getListing = (id) => listings.find((listing) => listing.id === id);
+
+const filterListings = (predicate) => listings.filter(predicate);
+
+module.exports = {
+  addListing,
+  getListings,
+  getListing,
+  filterListings,
+};
