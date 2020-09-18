a4c7412b4485d71757e97a164946df86883e3af3
Arun Krishanmoorthy <arunmba.rec@gmail.com>
2020-09-18 21:41:43 +0530

    Changes to service to update sorting function

--- store/listings.js
+++ store/listings.js
@@ -1,106 +1,144 @@
 const listings = [
   {
-    id: 201,
-    title: "Parker Pen",
-    images: [{ fileName: "parkerpen" }],
-    price: 100,
-    categoryId: 5,
-    userId: 1,
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
-  },
-  {
-    id: 3,
-    title: "Dymo",
-    images: [{ fileName: "dymo" }],
-    categoryId: 1,
-    price: 1200,
-    userId: 2,
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
-  },
-  {
-    id: 1,
-    title: "Elmers",
-    description:
-      "I'm selling my furniture at a discount price. Pick up at Venice. DM me asap.",
-    images: [
-      { fileName: "couch1" },
-      { fileName: "couch2" },
-      { fileName: "couch3" },
+    id: 401,
+    title: "Favorites",
+    data: [
+      {
+        id: 201,
+        title: "Parker Pen",
+        images: [
+          { fileName: "http://192.168.0.101:9000/assets/parkerpen_full.jpg" },
+        ],
+        price: 100,
+        categoryId: 5,
+        userId: 1,
+        location: {
+          latitude: 37.78825,
+          longitude: -122.4324,
+        },
+      },
+      {
+        id: 3,
+        title: "Dymo",
+        images: [
+          { fileName: "http://192.168.0.101:9000/assets/dymo_full.jpg" },
+        ],
+        categoryId: 1,
+        price: 1200,
+        userId: 2,
+        location: {
+          latitude: 37.78825,
+          longitude: -122.4324,
+        },
+      },
     ],
-    price: 1000,
-    categoryId: 1,
-    userId: 1,
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
   },
   {
-    id: 2,
-    title: "Mr. Sketch",
-    images: [{ fileName: "mrsketch" }],
-    categoryId: 5,
-    price: 100,
-    userId: 2,
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
+    id: 402,
+    title: "Writing",
+    data: [
+      {
+        id: 1,
+        title: "Elmers",
+        description:
+          "I'm selling my furniture at a discount price. Pick up at Venice. DM me asap.",
+        images: [
+          { fileName: "http://192.168.0.101:9000/assets/parkerpen_full.jpg" },
+          { fileName: "http://192.168.0.101:9000/assets/dymo_full.jpg" },
+          { fileName: "http://192.168.0.101:9000/assets/dymo_full.jpg" },
+        ],
+        price: 1000,
+        categoryId: 1,
+        userId: 1,
+        location: {
+          latitude: 37.78825,
+          longitude: -122.4324,
+        },
+      },
+      {
+        id: 2,
+        title: "Mr. Sketch",
+        images: [
+          { fileName: "http://192.168.0.101:9000/assets/mrsketch_full.jpg" },
+        ],
+        categoryId: 5,
+        price: 100,
+        userId: 2,
+        location: {
+          latitude: 37.78825,
+          longitude: -122.4324,
+        },
+      },
+      {
+        id: 102,
+        title: "Papermate",
+        images: [
+          { fileName: "http://192.168.0.101:9000/assets/dymo_full.jpg" },
+        ],
+        price: 300,
+        categoryId: 3,
+        userId: 1,
+        location: {
+          latitude: 37.78825,
+          longitude: -122.4324,
+        },
+      },
+    ],
   },
   {
-    id: 102,
-    title: "Papermate",
-    images: [{ fileName: "camera1" }],
-    price: 300,
-    categoryId: 3,
-    userId: 1,
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
+    id: 403,
+    title: "Home Appliances",
+    data: [
+      {
+        id: 101,
+        title: "Krazy Glue",
+        images: [
+          { fileName: "http://192.168.0.101:9000/assets/krazy_full.jpg" },
+        ],
+        price: 350,
+        categoryId: 3,
+        userId: 1,
+        location: {
+          latitude: 37.78825,
+          longitude: -122.4324,
+        },
+      },
+      {
+        id: 4,
+        title: "Prisma Color",
+        description: "No rips no stains no odors",
+        images: [
+          { fileName: "http://192.168.0.101:9000/assets/prismacolor_full.jpg" },
+        ],
+        categoryId: 1,
+        price: 950,
+        userId: 2,
+        location: {
+          latitude: 37.78825,
+          longitude: -122.4324,
+        },
+      },
+    ],
   },
   {
-    id: 101,
-    title: "Krazy Glue",
-    images: [{ fileName: "camera2" }],
-    price: 350,
-    categoryId: 3,
-    userId: 1,
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
-  },
-  {
-    id: 4,
-    title: "Prisma Color",
-    description: "No rips no stains no odors",
-    images: [{ fileName: "couch3" }],
-    categoryId: 1,
-    price: 950,
-    userId: 2,
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
-  },
-  {
-    id: 6,
-    title: "Reynolds",
-    images: [{ fileName: "shoes2" }],
-    categoryId: 5,
-    price: 50,
-    userId: 2,
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
+    id: 404,
+    title: "Security",
+    data: [
+      {
+        id: 6,
+        title: "Reynolds",
+        images: [
+          { fileName: "http://192.168.0.101:9000/assets/krazy_full.jpg" },
+        ],
+        categoryId: 5,
+        price: 50,
+        userId: 2,
+        location: {
+          latitude: 37.78825,
+          longitude: -122.4324,
+        },
+      },
+    ],
   },
 ];
 
